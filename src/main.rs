8a44dac106584f32146d4bc478def9d180ca1de5
chansuke <moonset20@gmail.com>
2017-10-16 20:02:32 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::net::TcpListner;
+use std::thread;
+use std::io::{Read, Write};
+use std::io;
+
+fn server_start() -> io::Result<()> {
+    let lis = TcpListner::bind("127.0.0.1:8080")?;
+
+    for stream in lis.incoming() {
+        let mut stream = match stream {
+            Ok(stream) => stream,
+            Err(e) => {
+                println!("An error occurred while accepting a connection: {}", e);
+                continue;
+            }
+        };
+
+        let _ = thread::spawn(
+            move || -> io::Result<()> {
+                loop {
+                    let mut b = [0; 1024];
+                    let n = stream.read(&mut b)?;
+                    if n == 0 {
+                        return Ok(());
+                    } else {
+                        stream.write(&b[0..n])?;
+                    }
+                }});
+    }
+    Ok(())
+}
+
+fn main() {
+    match server_start() {
+        Ok(_) => (),
+        Err(e) => println!("{:?}", e),
+    }
+}
